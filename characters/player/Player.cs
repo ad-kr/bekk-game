fd102d75a59447eb441d0cbe7821aa2c0e58412a
Adrian Kortyczko <bounce.adrian@gmail.com>
2022-09-23 22:38:19 +0200

    Add Player class

---
+++ characters/player/Player.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+namespace ADKR.Game
+{
+    public partial class Player : Character
+    {
+        public override void _Process(double delta)
+        {
+            base._Process(delta);
+
+            float xAxis = Input.GetAxis("ui_left", "ui_right");
+            float yAxis = Input.GetAxis("ui_up", "ui_down");
+
+            Vector2 dir = new Vector2(xAxis, yAxis).Normalized();
+
+            Velocity = dir * RunSpeed;
+
+            MoveAndSlide();
+        }
+    }
+}
