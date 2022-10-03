c6571a4ae2c7cdfc75c7a7f5a3eee18615635b90
Adrian Kortyczko <bounce.adrian@gmail.com>
2022-10-03 16:30:44 +0200

    Implement Combatable class

---
+++ characters/combatable/Combatable.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+
+namespace ADKR.Game
+{
+    public abstract partial class Combatable : Character
+    {
+        public static List<Combatable> Combatables { get; set; } = new();
+
+        private float _health = 100f;
+        public float Health
+        {
+            get => _health;
+            set
+            {
+                _health = value > MaxHealth ? MaxHealth : value;
+                if (_health <= 0f) Die();
+            }
+        }
+
+        public float MaxHealth { get; set; } = 100f;
+
+        public Faction Faction { get; set; }
+
+        public override void _Ready()
+        {
+            base._Ready();
+            Combatables.Add(this);
+        }
+
+        public virtual void Die()
+        {
+            Combatables.Remove(this);
+        }
+
+    }
+}
