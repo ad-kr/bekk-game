97ed857adc86c34bd0f2bb131fbce44d164379af
Adrian Kortyczko <bounce.adrian@gmail.com>
2022-10-04 19:31:59 +0200

    Godot doing its stuff....

--- characters/Character.cs
+++ characters/Character.cs
@@ -3,54 +3,59 @@ using System;
 
 namespace ADKR.Game
 {
-    public partial class Character : CharacterBody2D
-    {
+	public partial class Character : CharacterBody2D
+	{
 
-        public AnimatedSprite2D Sprite { get; set; }
+		public AnimatedSprite2D Sprite { get; set; }
 
-        public float RunSpeed { get; set; } = 64f;
+		public float RunSpeed { get; set; } = 64f;
 
-        private CharacterState _state;
+		private CharacterState _state;
 
-        public CharacterState State
-        {
-            get => _state;
-            set
-            {
-                _state?.End();
-                _state = value;
-                _state.SetCharacter(this);
-                _state?.Start();
-            }
-        }
+		public CharacterState State
+		{
+			get => _state;
+			set
+			{
+				_state?.End();
+				_state = value;
+				_state.SetCharacter(this);
+				_state?.Start();
+			}
+		}
 
-        private bool _isFlipped;
-        public bool IsFlipped
-        {
-            get => _isFlipped;
-            set
-            {
-                _isFlipped = value;
-                SetFlip(value);
-            }
-        }
+		private bool _isFlipped;
+		public bool IsFlipped
+		{
+			get => _isFlipped;
+			set
+			{
+				_isFlipped = value;
+				SetFlip(value);
+			}
+		}
 
-        public override void _Ready()
-        {
-            base._Ready();
-            Sprite = GetNode<AnimatedSprite2D>("Sprite");
-        }
+		public override void _Ready()
+		{
+			base._Ready();
+			Sprite = GetNode<AnimatedSprite2D>("Sprite");
+		}
 
-        public override void _Process(double delta)
-        {
-            base._Process(delta);
-            State?.Update(delta);
-        }
+		public override void _Process(double delta)
+		{
+			base._Process(delta);
+			State?.Update(delta);
+		}
 
-        protected virtual void SetFlip(bool isFlipped)
-        {
-            Sprite.FlipH = isFlipped;
-        }
+		public override void _PhysicsProcess(double delta){
+			base._PhysicsProcess(delta);
+			State?.PhysicsUpdate(delta);
+		}
 
-    }
+		protected virtual void SetFlip(bool isFlipped)
+		{
+			Sprite.FlipH = isFlipped;
+		}
+
+	}
 }
