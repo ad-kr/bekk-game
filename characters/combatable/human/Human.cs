2086ea601b73cfa5d37d9bce5389e9e3ac5cd809
Adrian Kortyczko <bounce.adrian@gmail.com>
2022-10-17 20:45:42 +0200

    Import skin, hair and clothes

--- characters/combatable/human/Human.cs
+++ characters/combatable/human/Human.cs
@@ -3,39 +3,42 @@ using System;
 
 namespace ADKR.Game
 {
-	public partial class Human : Combatable
-	{
+    public partial class Human : Combatable
+    {
 
-		public CharacterHand TopHand { get; set; }
-		public CharacterHand BottomHand { get; set; }
+        public CharacterHand TopHand { get; set; }
+        public CharacterHand BottomHand { get; set; }
+
+        public Sprite2D Hair { get; set; }
+        public Sprite2D Clothes { get; set; }
 
         public override void _Ready()
-		{
-			base._Ready();
+        {
+            base._Ready();
 
-			Texture2D handTexture = GD.Load<Texture2D>("res://characters/combatable/human/hand.png");
+            Texture2D handTexture = GD.Load<Texture2D>("res://characters/combatable/human/hand.png");
 
-			TopHand = new(handTexture);
-			BottomHand = new(handTexture);
+            TopHand = new(handTexture);
+            BottomHand = new(handTexture);
 
-			Sprite.AddChild(TopHand);
-			Sprite.AddChild(BottomHand);
+            Sprite.AddChild(TopHand);
+            Sprite.AddChild(BottomHand);
 
-			TopHand.Offset = 8f;
-			TopHand.Angle = 90f;
-			TopHand.PivotOffset = new Vector2(-2f, -2f);
+            TopHand.Offset = 8f;
+            TopHand.Angle = 90f;
+            TopHand.PivotOffset = new Vector2(-2f, -2f);
 
-			BottomHand.Offset = 8f;
-			BottomHand.Angle = 45f;
-			BottomHand.PivotOffset = new Vector2(-1f, -2f);
-			BottomHand.ZIndex = -1;
-		}
+            BottomHand.Offset = 8f;
+            BottomHand.Angle = 45f;
+            BottomHand.PivotOffset = new Vector2(-1f, -2f);
+            BottomHand.ZIndex = -1;
+        }
 
-		protected override void SetFlip(bool isFlipped)
-		{
-			base.SetFlip(isFlipped);
-			TopHand.IsFlipped = isFlipped;
-			BottomHand.IsFlipped = isFlipped;
-		}
-	}
+        protected override void SetFlip(bool isFlipped)
+        {
+            base.SetFlip(isFlipped);
+            TopHand.IsFlipped = isFlipped;
+            BottomHand.IsFlipped = isFlipped;
+        }
+    }
 }
